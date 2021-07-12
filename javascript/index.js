aa69ef9d60d0223291b6ce9204eb348ac5dfa35b
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-07-12 11:52:34 -0500

    arrancando con el curso de grid

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+const numeros = [1, 2, 3, 4, 5, 6, 4];
+numeros.map((numero) => {
+  if (numero == 4) {
+    // console.log(numero + "eso es la coincidencia");
+  } else {
+    console.log(numero);
+  }
+});
